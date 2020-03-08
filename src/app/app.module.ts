dc7841fe54cbd74421d16cbcaa122fa1ce2fd93a
穆星 <1327678113@qq.com>
2020-03-08 11:50:48 +0800

    init项目，并添加组件，搭建完框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SearchComponent } from "./search/search.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { ProductComponent } from "./product/product.component";
+import { StarsComponent } from "./stars/stars.component";
 
 @NgModule({
+  // ! 组件，指令，管道
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StarsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  //! 引入的angular模块
+  imports: [BrowserModule, FormsModule],
+  // ! 服务
   providers: [],
+  //! 主组件
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
