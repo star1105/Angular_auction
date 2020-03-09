516bd3d8f4d33a43f24df88aad0d9d83bd563d34
穆星 <1327678113@qq.com>
2020-03-09 21:00:05 +0800

    产品组件

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
+  public products: Array<Product>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.products = [
+      new Product(1, "华为p40", 6799, 1, "这是第一个商品的商品描述", [
+        "电子产品",
+        "硬件设备"
+      ]),
+      new Product(2, "华为p30", 6799, 3.5, "这是第二个商品的商品描述", [
+        "电子产品"
+      ]),
+      new Product(3, "华为mate 30", 6799, 4.5, "这是第三个商品的商品描述", [
+        "电子产品",
+        "硬件设备"
+      ]),
+      new Product(4, "华为mate 30 pro", 6799, 5, "这是第四个商品的商品描述", [
+        "电子产品"
+      ]),
+      new Product(
+        5,
+        "华为mate 30 pro 5G",
+        6799,
+        4,
+        "这是第五个商品的商品描述",
+        ["电子产品"]
+      ),
+      new Product(6, "华为mate 30 5G", 6799, 1, "这是第六个商品的商品描述", [
+        "电子产品",
+        "硬件设备"
+      ])
+    ];
+    this.products.push(
+      new Product(2, "华为p30", 6799, 3.5, "这是第二个商品的商品描述", [
+        "电子产品"
+      ])
+    );
   }
-
+}
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {}
 }
