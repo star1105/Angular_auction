c585011c043e393847c4a1aa997d5200bd985fa0
穆星 <1327678113@qq.com>
2020-04-06 13:15:37 +0800

    使用依赖注入service优化产品页面和产品详情页面

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Product } from "../product/product.component";
+import { ProductService, Comment } from "../shared/product.service";
 
 @Component({
   selector: "app-product-detail",
   templateUrl: "./product-detail.component.html",
-  styleUrls: ["./product-detail.component.css"]
+  styleUrls: ["./product-detail.component.css"],
 })
 export class ProductDetailComponent implements OnInit {
-  productTitle: string;
-  constructor(public routerInfo: ActivatedRoute) {}
+  product: Product;
+
+  comments: Comment[];
+
+  constructor(
+    public routerInfo: ActivatedRoute,
+    public productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-    this.productTitle = this.routerInfo.snapshot.params["prodTitle"];
+    let productId: number = this.routerInfo.snapshot.params["productId"];
+    this.product = this.productService.getProduct(productId);
+    this.comments = this.productService.getCommentsForProductId(productId);
   }
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,9 +1,72 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
+  public products: Product[] = [
+    new Product(1, "华为p40", 6799, 1, "这是第一个商品的商品描述", [
+      "电子产品",
+      "硬件设备",
+    ]),
+    new Product(2, "华为p30", 6799, 3.5, "这是第二个商品的商品描述", [
+      "电子产品",
+    ]),
+    new Product(3, "华为mate 30", 6799, 4.5, "这是第三个商品的商品描述", [
+      "电子产品",
+      "硬件设备",
+    ]),
+    new Product(4, "华为mate 30 pro", 6799, 5, "这是第四个商品的商品描述", [
+      "电子产品",
+    ]),
+    new Product(5, "华为mate 30 pro 5G", 6799, 4, "这是第五个商品的商品描述", [
+      "电子产品",
+    ]),
+    new Product(6, "华为mate 30 5G", 6799, 1, "这是第六个商品的商品描述", [
+      "电子产品",
+      "硬件设备",
+    ]),
+  ];
+  public comments: Comment[] = [
+    new Comment(1, 1, "2017-02-02 22:22:22", "star1", 3, "东西不错"),
+    new Comment(2, 1, "2017-02-02 22:22:22", "star2", 3, "东西不错"),
+    new Comment(3, 1, "2017-02-02 22:22:22", "star3", 3, "东西不错"),
+    new Comment(4, 1, "2017-02-02 22:22:22", "star4", 3, "东西不错"),
+    new Comment(5, 1, "2017-02-02 22:22:22", "star5", 3, "东西不错"),
+    new Comment(6, 2, "2017-02-02 22:22:22", "star6", 3, "东西不错"),
+  ];
+  constructor() {}
+  getProducts(): Product[] {
+    return this.products;
+  }
 
-  constructor() { }
+  getProduct(id: number): Product {
+    return this.products.find((product) => product.id == id);
+  }
+
+  getCommentsForProductId(id: number): Comment[] {
+    return this.comments.filter((comment: Comment) => comment.productId == id);
+  }
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {}
+}
+
+export class Comment {
+  constructor(
+    public id: number,
+    public productId: number,
+    public timestamp: string,
+    public user: string,
+    public rating: number,
+    public content: string
+  ) {}
 }
